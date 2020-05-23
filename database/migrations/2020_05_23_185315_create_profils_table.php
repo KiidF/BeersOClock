e239dfaa7972005a1c617f2ac0e8a2096022651f
lercat <ddbieres@gmail.com>
2020-05-23 21:01:18 +0200

    rajout table profils

---
+++ database/migrations/2020_05_23_185315_create_profils_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profils', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('titre')->nullable;
+            $table->text('presentation')->nullable;
+            $table->string('url')->nullable;
+            $table->timestamps();
+
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profils');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,10 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="assets/img/cheers.jpg" class="rounded-circle">
+            <img src="../../assets/img/cheers.jpg" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>BeersOClock</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>

1bd28ecd369a8f30530e2201b74852871959afb2
lercat <ddbieres@gmail.com>
2020-05-23 20:13:02 +0200

    table users + username

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,33 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="assets/img/cheers.jpg" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>BeersOClock</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">BeersOClock.com</div>
+            <div>Nous sommes une communauté de millions de buveurs de bières. Ce blog est open source pour tout ceux qui veulent apporter leur contribution (3) non profit ou pas !!!</div>
+            <div> <a href="#">www.ddbieres.com</a> </div>
         </div>
     </div>
-</div>
+        <!--les posts-->
+        <div class="row pt-5">
+            <div class="col-4">
+                <img src="assets/img/barbar.png" class="w-60" alt="bière barbar">
+            </div>
+            <div class="col-4">
+                <img src="assets/img/bernardus.png" class="w-60" alt="bière bernardus">
+            </div>
+            <div class="col-4">
+                <img src="assets/img/erdinger.png" class="w-60" alt="bière erdinger">
+            </div>
+        </div>
+</div>  
 @endsection
